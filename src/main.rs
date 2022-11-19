2391c6e6839644214cb2e3cd8634e353515c1f8d
Mike Pepper <mdcpepper@gmail.com>
2022-11-19 19:12:12 +0000

    Bind server to random available port in integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind available port");
+
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our server
-    run()?.await
+    run(listener)?.await
 }
