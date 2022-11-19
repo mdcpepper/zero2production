21c0ac6721de046c29c036ac46d74e603f399ff5
Mike Pepper <mdcpepper@gmail.com>
2022-11-19 22:13:45 +0000

    Save valid subscriptions to a Postgres database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -5,10 +6,11 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to PostgreSQL");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind available port");
 
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our server
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,15 +1,31 @@
 use actix_web::{web, HttpResponse};
-use serde::Deserialize;
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
-#[derive(Deserialize)]
-pub struct FormData {
+#[derive(serde::Deserialize)]
+pub struct SubscriptionData {
     email: String,
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-    let _name = &form.name;
-    let _email = &form.email;
-
-    HttpResponse::Ok().finish()
+pub async fn subscribe(form: web::Form<SubscriptionData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query:{}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
