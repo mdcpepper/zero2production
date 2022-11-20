1a5c0a582619fd049dca7f33c79927f5cef0dbed
Mike Pepper <mdcpepper@gmail.com>
2022-11-20 02:33:18 +0000

    Make sure database password is not exposed in telemetry

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
 use once_cell::sync::Lazy;
 use reqwest::Client;
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -120,16 +121,17 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_database())
-        .await
-        .expect("Failed to connect to PostgreSQL");
+    let mut connection =
+        PgConnection::connect(config.connection_string_without_database().expose_secret())
+            .await
+            .expect("Failed to connect to PostgreSQL");
 
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create test database");
 
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to PostgreSQL");
 
